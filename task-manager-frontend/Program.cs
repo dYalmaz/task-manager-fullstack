b59d10f421bf195368b81ecc6f9c4709239ba177
dYalmaz <120399269+dYalmaz@users.noreply.github.com>
2026-01-03 20:37:55 +0000

    change localhost to render

--- task-manager-frontend/Program.cs
+++ task-manager-frontend/Program.cs
@@ -6,6 +6,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri("https://task-manager-wy2i.onrender.com/") 
+});
 
 await builder.Build().RunAsync();
